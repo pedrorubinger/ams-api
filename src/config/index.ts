bad8f066edfa0beaca85b9c39645a60277f021c8
Pedro Rubinger <pedro.rubinger@gmail.com>
2022-12-18 11:37:23 -0300

    chore: adjust env

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
+/** SERVER */
+export const PORT = process.env.PORT
+export const HOST = process.env.HOST
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
 import express from "express"
-import dotenv from "dotenv"
 
-dotenv.config()
+import { HOST, PORT } from "@config"
 
 const app = express()
-const port = process.env.PORT
-const host = process.env.HOST
 
 app.use(express.json())
-app.listen(port, () =>
-  console.log(`[ON] Server is running at ${host} on port ${port}.`)
-)
+app.listen(PORT, () => console.log(`[ON] Server is running at ${HOST}:${PORT}`))
